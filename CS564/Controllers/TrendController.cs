a88e7ee338f59ed189a4e867066668f388425973
hylianhideaway <31197883+hylianhideaway@users.noreply.github.com>
2020-07-19 19:24:00 -0500

    Update TrendController to parse query result
    Now pareses query result into multiple outputs. Top 5, Mammals, Birds, and Reptiles.

--- CS564/Controllers/TrendController.cs
+++ CS564/Controllers/TrendController.cs
@@ -1,4 +1,4 @@
-﻿using CS564.Database;
+using CS564.Database;
 using CS564.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -26,7 +26,47 @@ namespace CS564.Controllers
         [Route("")]
         public ActionResult<IEnumerable<Trend>> TrendObservations(DateTime date, int locationID)
         {
-            return Ok(_context.GetAllTrendsMatchingCriteria(date, locationID));
+            IEnumerable<Trend> allTrends = _context.GetAllTrendsMatchingCriteria(date, locationID);
+
+            List<Trend> top5Animals = new List<Trend>();
+            List<Trend> mammals = new List<Trend>();
+            List<Trend> birds = new List<Trend>();
+            List<Trend> reptiles = new List<Trend>();
+
+            int Top5Counter = 5; 
+            Trend currentTrend = null; 
+            for (int i = 0; i < allTrends.Count(); i++)
+            {
+                currentTrend = allTrends.ElementAt(i);
+
+                if (Top5Counter > 0)
+                {
+                    top5Animals.Add(currentTrend);
+                    Top5Counter--;
+                }
+
+                if (currentTrend.Class.Equals("Mammalia"))
+                {
+                    mammals.Add(currentTrend);
+                }
+                else if (currentTrend.Class.Equals("Aves"))
+                {
+                    birds.Add(currentTrend); 
+                } 
+                else if (currentTrend.Class.Equals("Reptilia"))
+                {
+                    reptiles.Add(currentTrend); 
+                }
+            }
+
+            return Ok( new
+            {
+                top5Animals ,
+                mammals , 
+                birds ,
+                reptiles
+            }   
+                );
         }
     }
 }
