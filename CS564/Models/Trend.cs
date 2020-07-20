46025609324e3f24fae96e5f36a6ca81122462ee
hylianhideaway <31197883+hylianhideaway@users.noreply.github.com>
2020-07-19 19:25:58 -0500

    Added  Class method
    Class method created. Used to filter results of Query to specific taxonomic classes on the server side. Eliminates the need for multiple queries.

--- CS564/Models/Trend.cs
+++ CS564/Models/Trend.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 
 namespace CS564.Models
 {
@@ -16,5 +17,8 @@ namespace CS564.Models
         public string Animal { get; set; }
 
         public int Trending { get; set; }
+
+        public string Class { get; set; }
+
     }
 }
